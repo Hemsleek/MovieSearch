a38ba7eb0ae31fb04aea3ec02ca6ea6dac2c733e
hemsleek <asiyanbimubashir@gmail.com>
2020-10-24 20:27:17 +0100

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,54 @@ import React from 'react';
 
 import './App.scss';
 
-const App= () =>
- (
-    <div className="App bg-gray-300 p-4">
-      <input type="search" className="px-2 py-1 block mx-auto mt-4 rounded outline-none" />
+const Nav = () => {
+  const navbarTabs = "IMBD Rating,Latest,Popular,Most Downloaded".split(',')
+  const categories ="Category,All,Action,Sci-Fi,Comedy,Tragedy,Horror".split(',')
+
+    return (
+      
+      <nav className="Nav w-full flex items-center h-12 px-4 bg-gray-600 text-white">
+
+        <span className="brand-logo cursor-pointer text-lg font-bold">MoviesMight</span>
+
+        <input type="search" className="px-2 w-48 py-1 ml-3 rounded outline-none text-black text-opacity-75" />
+
+        <span className="spacer flex-grow"></span>
+
+        {
+          navbarTabs.map((tab , tabIndex) => <span className="tab ml-3 cursor-pointer" key={`navTab-${tabIndex}`} >{tab}</span>)
+        }
+
+        <span className="spacer flex-grow"></span>
+        <select defaultValue="Category" className="categories outline-none px-4 py-2 bg-white  text-black text-opacity-75 rounded-md">
+          
+          {
+            categories.map((category, categoryIndex) => 
+            <option key={`category-${categoryIndex}`}  
+              disabled={categoryIndex===0? true : false} >
+
+              {category}
+
+            </option>)
+          }
+        </select>
+
+      </nav>
+    )
+}
+
+const App= () =>{
+
+
+ return(
+    <div className="App bg-gray-200 flex flex-col w-screen h-screen">
+      <Nav />
+     
+      <div className="Screen">
+
+      </div>
     </div>
   ); 
-
+ }
 
 export default App;
