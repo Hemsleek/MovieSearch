a4ace928aa7457cb982a36eff3719a40a71888cb
hemsleek <asiyanbimubashir@gmail.com>
2020-11-15 03:16:05 +0100

    feat: HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -64,8 +64,8 @@ const Movies = ({movies}) => {
 const HomePage = () =>{
 
   return(
-    <main className="flex-grow flex flex-col items-center justify-center">
-      <div className="brand-logo text-5xl text-center text-indigo-900">Welcome To The <br />MoviesMight</div>
+    <main className="flex-grow flex flex-col items-center">
+      <div className="brand-logo text-5xl text-center mt-32 text-indigo-900">Welcome To The <br />MoviesMight</div>
       <form>
         <input type="search" className="px-3 py-2 border-none shadow-md rounded-lg  focus:outline-none " placeholder="Search Movies"/>
       </form>
